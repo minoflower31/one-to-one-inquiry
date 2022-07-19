884873cd223b2866a10f5a77f79d2c9ca9a17810
minoflower31@naver.com <minoflower31@naver.com>
2022-07-19 21:36:40 +0900

    fix: Delete to mapping method about spring interceptor.

--- src/main/java/com/fastcampus/ToyProject/web/controller/LoginController.java
+++ src/main/java/com/fastcampus/ToyProject/web/controller/LoginController.java
@@ -1,20 +1,10 @@
 package com.fastcampus.ToyProject.web.controller;
 
-import com.fastcampus.ToyProject.domain.User;
-import com.fastcampus.ToyProject.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequiredArgsConstructor
-public class UserController {
-
-    private final UserService userService;
+public class LoginController {
 
     @GetMapping("/auth/login")
     public String loginView() {
