4f811f3a5471ae11279205e19ffaff0544c7edc3
minoflower31@naver.com <minoflower31@naver.com>
2022-07-19 21:34:52 +0900

    fix: Change the file name. UserController.java to LoginController.java

--- src/main/java/com/fastcampus/ToyProject/web/controller/LoginController.java
+++ src/main/java/com/fastcampus/ToyProject/web/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.fastcampus.ToyProject.domain.User;
 import com.fastcampus.ToyProject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,25 +15,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UserController {
 
     private final UserService userService;
-
-    @GetMapping("/auth/insertUser")
-    public String insertUserView() {
-        return "user/insertUser";
-    }
-
-    @PostMapping("/auth/insertUser")
-    @ResponseBody
-    public String insertUser(@RequestBody User user) {
-        //username으로 검증
-        User findUser = userService.getUser(user.getUsername());
-
-        if (findUser.getUsername() == null) {
-            userService.insertUser(user);
-            return user.getUsername() + " 회원 가입 성공";
-        } else {
-            return findUser.getUsername() + " -> 이미 등록된 회원입니다.";
-        }
-    }
 
     @GetMapping("/auth/login")
     public String loginView() {
